7a6e0419ac8d28b297cbf939be44fe2b608ffd72
pgrod <149545353+pgrod@users.noreply.github.com>
2025-12-06 19:01:36 -0800

    jwt

--- CitasMedicasBackend/server.js
+++ CitasMedicasBackend/server.js
@@ -7,5 +7,9 @@ require('dotenv').config();
 const pool = require('./config/bd');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const JWT_SECRET = process.env.JWT_SECRET
+
+app.use(cors());
+app.use(express.json());
+
+
+const JWT_SECRET = process.env.JWT_SECRET || 'la_clave_secreta_para_jwt';
